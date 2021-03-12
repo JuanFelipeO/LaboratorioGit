64dbcf80e53028dd625dae90b0578197119d6499
Juan Felipe Oriz <jfortiz87911@umanizales.edu.co>
2021-03-11 21:18:19 -0500

    Se organiza el error que se tenia con el ciclo FOR en la clase IngresarDatos, se deja funcionando de forma adecuada.

--- src/laboratoriogit/IngresarPersona.java
+++ src/laboratoriogit/IngresarPersona.java
@@ -20,27 +20,30 @@ public class IngresarPersona implements Serializable
     
     public void IngresarDatos()
     {
-       String teclado;
+      
        Scanner entradaEscaner = new Scanner (System.in);
-       teclado = entradaEscaner.nextLine();
+ 
        
        for(int i=3; i>0; i--)
        {
            System.out.println("Ingrese el nombre de la persona:");
-           nombre = teclado;
+           nombre = entradaEscaner.nextLine();
            nombres.add(nombre);
            System.out.println("Ingrese el telefono de la persona ingresada:");
-           telefono = teclado;
+           telefono = entradaEscaner.nextLine();;
            telefonos.add(telefono);
            System.out.println("Ingrese el # de C.C de la persona ingresada:");
-           cedula = teclado;
+           cedula = entradaEscaner.nextLine();;
            cedulas.add(cedula);
        }
-       
+        
+        System.out.println("");
         System.out.println("Lista de nombres ingresados:");
         System.out.println(nombres.toString());
+        System.out.println("");
         System.out.println("Telefonso de las personas ingresadas:");
         System.out.println(telefonos.toString());
+        System.out.println("");
         System.out.println("Cedulas de las personas ingresadas:");
         System.out.println(cedulas.toString());
     }
